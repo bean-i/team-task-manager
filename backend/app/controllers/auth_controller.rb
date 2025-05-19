0288de305ec2becf103d2a5f39e7a385699188fa
been <leebin0603@naver.com>
2025-05-19 16:26:08 +0900

    [refactor] 認証エラー処理の改善

--- backend/app/controllers/auth_controller.rb
+++ backend/app/controllers/auth_controller.rb
@@ -22,22 +22,23 @@ class AuthController < ApplicationController
   # Log In
   def login
     user = User.find_by(email: login_params[:email])
-
+    
     if user&.authenticate(login_params[:password])
-      token = JsonWebToken.encode(user_id: user.id)
+      token = JWT.encode(
+        {
+          user_id: user.id,
+          exp: 24.hours.from_now.to_i
+        },
+        Rails.application.credentials.secret_key_base,
+        'HS256'
+      )
+      
       render_success(
         message: "ログインに成功しました",
-        data: {
-          user: user,
-          token: token,
-        },
+        data: { user: user, token: token }
       )
     else
-      render_error(
-        message: "メールまたはパスワードが正しくありません",
-        code: 401,
-        status: :unauthorized,
-      )
+      raise CustomErrors::AuthenticationError.new('メールアドレスまたはパスワードが正しくありません')
     end
   end
 
