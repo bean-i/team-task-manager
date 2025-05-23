326aaf11e95705044221c75e7e258db2db2e6feb
been <leebin0603@naver.com>
2025-05-23 21:09:54 +0900

    [chore] ワークスペースの更新・削除APIルートを削除

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
   post '/signup', to: 'auth#signup'
   post '/login', to: 'auth#login'
   
-  resources :workspaces do
+  resources :workspaces, only: [:index, :show, :create] do
     collection do
       get :available
     end
     member do
       post :join
-      get :progress
-      get :member_progress
       get :progress_summary
     end
     resources :tasks, only: [:create, :update, :destroy]
